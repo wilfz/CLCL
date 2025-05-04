5cf1e58df8ba032d3728635873e8b52ea37f6cce
wilfz <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>
2025-05-04 23:56:23 +0200

    added tool_utl(japanese).txt, tool_utl(english).txt, string resources

--- CLCLPlugin/tool_utl/resource.h
+++ CLCLPlugin/tool_utl/resource.h
@@ -1,11 +1,23 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by tool_utl.rc
 //
 #define IDD_DIALOG_DATE_TIME_FORMAT     101
 #define IDD_DIALOG_QUOTE                102
 #define IDD_DIALOG_JOIN_TEXT            103
 #define IDD_DIALOG_MULTI_SAVE           104
+#define IDS_CLEAR_HISTORY               105
+#define IDS_CLEAR_CLIPBOARD             106
+#define IDS_PLAY_SOUND                  107
+#define IDS_ALWAYS_ON_TOP               108
+#define IDS_UN_TOP                      109
+#define IDS_SAVE_SEVERAL                110
+#define IDS_CLR_HIST_CAPTION            111
+#define IDS_CLR_HIST_QUESTION           112
+#define IDS_WAVE_FILE                   113
+#define IDS_SEL_OUTPUT_FOLDER           114
+#define IDS_CAPTION_OUTPUT              115
+#define IDS_SAVE_FAILED                 116
 #define IDC_COMBO_DATE                  1002
 #define IDC_COMBO_TIME                  1003
 #define IDC_EDIT_QUOTE_CHAR             1003
@@ -21,7 +33,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        117
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
